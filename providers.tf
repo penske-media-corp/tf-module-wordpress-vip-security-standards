996f918dbd2a59aaff7c0078a8e1f52106d3ecb1
Malik Cameron <malikcameron95@gmail.com>
2024-04-17 11:13:00 -0700

    Adding github provider

--- providers.tf
+++ providers.tf
@@ -5,4 +5,12 @@ terraform {
       version = "~> 5.0.0"
     }
   }
+  provider "github" {
+  owner = "penske-media-corp"
+  app_auth {
+    id              = var.github_app_id
+    installation_id = var.github_app_installation_id
+    pem_file        = var.github_app_pem_file
+  }
+}
 }
